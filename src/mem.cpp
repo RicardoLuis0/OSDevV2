84383a68c870ab1b19f80631b837323b3b2619e2
RicardoLuis0 <ricolvs123@gmail.com>
2019-12-12 17:43:17 -0300

    misc

--- src/mem.cpp
+++ src/mem.cpp
@@ -11,35 +11,30 @@ Spinlock memory_lock;
 
 struct gdt_entry{
     uint8_t arr[8];
+    inline void encode(uint32_t base, uint32_t limit, uint32_t type){
+        if ((limit > 65536) && ((limit & 0xFFF) != 0xFFF)) {
+            k_abort_s("Invalid limit for GDT entry!");
+        }
+        if (limit > 65536) {
+            // Adjust granularity if required
+            limit = limit >> 12;
+            arr[6] = 0xC0;
+        } else {
+            arr[6] = 0x40;
+        }
+        // Encode the limit
+        arr[0] = limit & 0xFF;
+        arr[1] = (limit >> 8) & 0xFF;
+        arr[6] |= (limit >> 16) & 0xF;
+        // Encode the base 
+        arr[2] = base & 0xFF;
+        arr[3] = (base >> 8) & 0xFF;
+        arr[4] = (base >> 16) & 0xFF;
+        arr[7] = (base >> 24) & 0xFF;
+        // And... Type
+        arr[5] = type;
+    }
 };
-
-static inline gdt_entry encodeGdtEntry(uint32_t base, uint32_t limit, uint32_t type) {
-    //adapted from https://wiki.osdev.org/GDT_Tutorial, accessed on decembeer 5 2019
-    // Check the limit to make sure that it can be encoded
-    gdt_entry target;
-    if ((limit > 65536) && ((limit & 0xFFF) != 0xFFF)) {
-        k_abort_s("Invalid limit for GDT entry!");
-    }
-    if (limit > 65536) {
-        // Adjust granularity if required
-        limit = limit >> 12;
-        target.arr[6] = 0xC0;
-    } else {
-        target.arr[6] = 0x40;
-    }
-    // Encode the limit
-    target.arr[0] = limit & 0xFF;
-    target.arr[1] = (limit >> 8) & 0xFF;
-    target.arr[6] |= (limit >> 16) & 0xF;
-    // Encode the base 
-    target.arr[2] = base & 0xFF;
-    target.arr[3] = (base >> 8) & 0xFF;
-    target.arr[4] = (base >> 16) & 0xFF;
-    target.arr[7] = (base >> 24) & 0xFF;
-    // And... Type
-    target.arr[5] = type;
-    return target;
-}
 
 gdt_entry GDT[3];
 
@@ -121,10 +116,10 @@ void build_first(multiboot_memory_map_t * mmap){
 
 void Memory::init(struct multiboot_info * mbd){
     print(" -Initializing Memory...\n -Loading GDT...\n");
-    GDT[0]=encodeGdtEntry(0,0,0);
-    GDT[1]=encodeGdtEntry(0,0xFFFFFFFF,0x9A);
-    GDT[2]=encodeGdtEntry(0,0xFFFFFFFF,0x92);
-    //GDT[3]=encodeGdtEntry(???,sizeof(???),0x89), //???=TSS location, 0x18
+    GDT[0].encode(0,0,0);
+    GDT[1].encode(0,0xFFFFFFFF,0x9A);
+    GDT[2].encode(0,0xFFFFFFFF,0x92);
+    //GDT[3].encode(???,sizeof(???),0x89), //???=TSS location, 0x18
     loadgdt((uint32_t)GDT,sizeof(GDT));//load flat 4GB GDT
     print(" -Parsing Memory Map...\n");
     uint64_t total=0;
