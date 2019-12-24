27ab6282e461d8934c8c68d4791619341d95a917
RicardoLuis0 <ricolvs123@gmail.com>
2019-12-24 18:24:56 -0300

    array of asm interrupt entrypoints

--- src/idt.cpp
+++ src/idt.cpp
@@ -1,6 +1,24 @@
 #include "idt.h"
 #include "default/stdint.h"
 
+#define IDTX(n) extern "C" void _idtx##n();
+#define IDTXi() IDTX(0) IDTX(1) IDTX(2) IDTX(3) IDTX(4) IDTX(5) IDTX(6) IDTX(7) IDTX(8) IDTX(9)
+#define IDTXd(n) IDTX(n##0) IDTX(n##1) IDTX(n##2) IDTX(n##3) IDTX(n##4) IDTX(n##5) IDTX(n##6) IDTX(n##7) IDTX(n##8) IDTX(n##9)
+#define IDTXdi() IDTXi() IDTXd(1) IDTXd(2) IDTXd(3) IDTXd(4) IDTXd(5) IDTXd(6) IDTXd(7) IDTXd(8) IDTXd(9)
+#define IDTXdd(n) IDTXd(n##0) IDTXd(n##1) IDTXd(n##2) IDTXd(n##3) IDTXd(n##4) IDTXd(n##5) IDTXd(n##6) IDTXd(n##7) IDTXd(n##8) IDTXd(n##9)
+#define IDTXv(n) IDTX(n##0) IDTX(n##1) IDTX(n##2) IDTX(n##3) IDTX(n##4) IDTX(n##5)
+#define IDTXdv(n) IDTXd(n##0) IDTXd(n##1) IDTXd(n##2) IDTXd(n##3) IDTXd(n##4) IDTXv(n##5)
+#define IDTXa() IDTXdi() IDTXdd(1) IDTXdv(2)
+
+#define listIDTX(n) _idtx##n,
+#define listIDTXi() listIDTX(0) listIDTX(1) listIDTX(2) listIDTX(3) listIDTX(4) listIDTX(5) listIDTX(6) listIDTX(7) listIDTX(8) listIDTX(9)
+#define listIDTXd(n) listIDTX(n##0) listIDTX(n##1) listIDTX(n##2) listIDTX(n##3) listIDTX(n##4) listIDTX(n##5) listIDTX(n##6) listIDTX(n##7) listIDTX(n##8) listIDTX(n##9)
+#define listIDTXdi() listIDTXi() listIDTXd(1) listIDTXd(2) listIDTXd(3) listIDTXd(4) listIDTXd(5) listIDTXd(6) listIDTXd(7) listIDTXd(8) listIDTXd(9)
+#define listIDTXdd(n) listIDTXd(n##0) listIDTXd(n##1) listIDTXd(n##2) listIDTXd(n##3) listIDTXd(n##4) listIDTXd(n##5) listIDTXd(n##6) listIDTXd(n##7) listIDTXd(n##8) listIDTXd(n##9)
+#define listIDTXv(n) listIDTX(n##0) listIDTX(n##1) listIDTX(n##2) listIDTX(n##3) listIDTX(n##4) listIDTX(n##5)
+#define listIDTXdv(n) listIDTXd(n##0) listIDTXd(n##1) listIDTXd(n##2) listIDTXd(n##3) listIDTXd(n##4) listIDTXv(n##5)
+#define listIDTXa() listIDTXdi() listIDTXdd(1) listIDTXdv(2)
+
 using namespace IDT;
 
 namespace IDT {
@@ -64,14 +82,17 @@ extern "C" void call_idt(int num){
     }
 }
 
+IDTXa()
+
+constexpr void(*idtx[256])(){//array of asm idt entrypoints
+    listIDTXa()
+};
+
 void IDT::init(){
     //TODO
 }
 
 void IDT::set_handler(uint8_t num,void(*c)(void)){
-    if(num<32){
-        return;//can't change reserved callbacks
-    }else{
-        idt_callback[num]=c;
-    }
+    if(num<32)return;
+    idt_callback[num]=c;
 }
