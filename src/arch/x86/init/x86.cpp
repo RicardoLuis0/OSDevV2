47fa34e99c5929cd902d35117499fce974e06275
RicardoLuis0 <ricolvs123@gmail.com>
2020-08-07 09:10:47 -0300

    formatting

--- src/arch/x86/init/x86.cpp
+++ src/arch/x86/init/x86.cpp
@@ -12,36 +12,37 @@ static __attribute__ ((constructor)) void global_constructors_ok(void) {
 #endif // DEBUG
 
 extern "C" {
+    
     void outb(uint16_t port, uint8_t val) {
         asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
     }
-
+    
     uint8_t inb(uint16_t port) {
         uint8_t ret;
         asm volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
         return ret;
     }
-
+    
     void outw(uint16_t port, uint16_t val){
         asm volatile ( "outw %0, %1" : : "a"(val), "Nd"(port) );
     }
-
+    
     uint16_t inw(uint16_t port){
         uint16_t ret;
         asm volatile ( "inw %1, %0" : "=a"(ret) : "Nd"(port) );
         return ret;
     }
-
+    
     void outl(uint16_t port, uint32_t val){
         asm volatile ( "outl %0, %1" : : "a"(val), "Nd"(port) );
     }
-
+    
     uint32_t inl(uint16_t port){
         uint32_t ret;
         asm volatile ( "inl %1, %0" : "=a"(ret) : "Nd"(port) );
         return ret;
     }
-
+    
 }
 
 extern "C" void _init();
