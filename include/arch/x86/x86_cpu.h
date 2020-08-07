13acaace4fa4827dba2aecc8c2b9015c700be756
RicardoLuis0 <ricolvs123@gmail.com>
2020-08-07 15:21:25 -0300

    CPU::switch_state stub

--- include/arch/x86/x86_cpu.h
+++ include/arch/x86/x86_cpu.h
@@ -2,11 +2,16 @@
 #define X86_CPU_H_INCLUDED
 
 namespace CPU {
+    
     struct CpuState{
         //TODO implement
     }__attribute__((packed));
+    
     extern "C" [[noreturn]] void enter_state(CpuState*);
+    extern "C" void switch_state(CpuState * from,CpuState * to);//switch to other state and save current state
+    
     void generate_state(CpuState * data,void *(*start)(void*) ,void * stack, void * page_directory);
+    
 }
 
 #endif // X86_CPU_H_INCLUDED
--- src/arch/x86/tasking/cpu.asm
+++ src/arch/x86/tasking/cpu.asm
@@ -1,10 +1,12 @@
 [BITS 32]
 section .rodata
 unimp:db "CPU::enter_state unimplemented",0
+unimp2:db "CPU::switch_state unimplemented",0
 
 section .text
 
 extern k_abort_s
+;void enter_state(CpuState *);
 global enter_state
 enter_state:
     push unimp
@@ -12,3 +14,12 @@ enter_state:
     ;TODO
     ret
     
+;void switch_state(CpuState * from,CpuState * to);
+global switch_state
+switch_state:
+    push unimp2
+    call k_abort_s
+    ;TODO
+    ret
+
+
