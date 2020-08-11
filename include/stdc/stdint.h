db1aaeba1be26d320cdc5b0ca0483992e01546d8
RicardoLuis0 <ricolvs123@gmail.com>
2020-08-11 12:21:21 -0300

    include stddef wtih stdint

--- include/stdc/stdint.h
+++ include/stdc/stdint.h
@@ -4,6 +4,8 @@
 #define join(a,b) a##b
 #define join_indirect(a,b) join(a,b)
 
+#include <stddef.h>
+
 typedef __INT8_TYPE__ int8_t;
 typedef __INT16_TYPE__ int16_t;
 typedef __INT32_TYPE__ int32_t;
