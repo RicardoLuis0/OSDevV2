4e9cc3fe5df7b6237df038561d79bb43e31bc28b
RicardoLuis0 <ricolvs123@gmail.com>
2020-08-12 14:35:35 -0300

    ask for video info from grub

--- src/arch/x86/init/boot.asm
+++ src/arch/x86/init/boot.asm
@@ -2,7 +2,9 @@
 ;https://wiki.osdev.org/Bare_Bones_with_NASM
     MBALIGN     equ  1 << 0            ; align loaded modules on page boundaries
     MEMINFO     equ  1 << 1            ; provide memory map
-    FLAGS       equ  MBALIGN | MEMINFO ; this is the Multiboot 'flag' field
+    VIDINFO     equ  1 << 2            ; provide video mode info
+    FLAGS       equ  MBALIGN | MEMINFO | VIDINFO
+                                       ; this is the Multiboot 'flag' field
     MAGIC       equ  0x1BADB002        ; 'magic number' lets bootloader find the header
     CHECKSUM    equ  -(MAGIC + FLAGS)  ; checksum of above, to prove we are multiboot
 ;    STACK_SIZE  equ  32768            ; 32K stack
