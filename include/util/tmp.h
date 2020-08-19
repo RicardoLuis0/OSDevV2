0e5304751e4a86fbfcdca99ec39e30080e8a9842
RicardoLuis0 <ricolvs123@gmail.com>
2020-08-19 03:13:38 -0300

    is_function, is_member_function_pointer (might not be ideal implementations, taken from cppreference)

--- include/util/tmp.h
+++ include/util/tmp.h
@@ -3,68 +3,19 @@
 
 #include "stdc/stddef.h"
 
+#include "util/tmp/base.h"
+#include "util/tmp/is_function.h"
+
 namespace TMP {
     
-    struct true_type {static constexpr bool value = true;};
+    template< class T >
+    struct is_member_function_pointer_helper : false_type {};
     
-    struct false_type {static constexpr bool value = false;};
+    template<class T, class U>
+    struct is_member_function_pointer_helper<T U::*> : is_function<T> {};
     
-    template<typename T> struct type_container{using type=T;};
-    
-    template<typename T> struct remove_const:type_container<T>{};
-    template<typename T> struct remove_const<const T>:type_container<T>{};
-    
-    template<typename T> struct remove_volatile:type_container<T>{};
-    template<typename T> struct remove_volatile<volatile T>:type_container<T>{};
-    
-    template<typename T> struct remove_cv:type_container<typename remove_const<typename remove_volatile<T>::type>::type>{};
-    
-    template<typename T> struct remove_reference:type_container<T>{};
-    template<typename T> struct remove_reference<T&>:type_container<T>{};
-    template<typename T> struct remove_reference<T&&>:type_container<T>{};
-    
-    template<typename T,typename U> struct is_same:false_type{};
-    
-    template<typename T> struct is_same<T,T>:true_type{};
-    
-    template<typename T> struct is_null_pointer:is_same<typename remove_cv<T>::type,nullptr_t>{};
-    
-    template<typename> struct is_pointer:false_type{};
-    template<typename T> struct is_pointer<T*>:true_type{};
-    
-    template<typename T>
-    constexpr bool is_null_pointer_v = is_null_pointer<T>::value;
-    
-    template<typename T>
-    constexpr bool is_pointer_v = is_pointer<T>::value;
-    
-    template<auto V>
-    constexpr bool is_null_pointer_dv = is_null_pointer<decltype(V)>::value;
-    
-    template<typename T>
-    constexpr T&& forward(typename remove_reference<T>::type& t) noexcept {
-        return static_cast<T&&>(t);
-    }
-    
-    template <typename T>
-    constexpr T&& forward(typename remove_reference<T>::type&& t) noexcept {
-        return static_cast<T&&>(t);
-    }
-    
-    template <typename T>
-    typename remove_reference<T>::type&& move(T&& t) {
-      return static_cast<typename remove_reference<T>::type&&>(t);
-    }
-    
-    template<bool,typename T=void> struct enable_if {};
-    template<typename T> struct enable_if<true, T> {using type=T;};
-    
-    template<bool B,typename T,typename F> struct conditional{using type=T;};
-    template<typename T,typename F> struct conditional<false,T,F>{using type=F;};
-    
-    template<class...> struct conjunction : true_type {};
-    template<class T> struct conjunction<T> : T {};
-    template<class T,class... Tn> struct conjunction<T, Tn...> : conditional<T::value, conjunction<Tn...>, T> {};
+    template<class T>
+    struct is_member_function_pointer : is_member_function_pointer_helper< typename remove_cv<T>::type > {};
     
 }
 
